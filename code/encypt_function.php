00323f6d381674c2047ac99ceb1c07347fd7a3e3
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-28 11:42:50 +0900

    Create encypt_function.php

---
+++ code/encypt_function.php
@@ -0,0 +1,4 @@
+    function encypt($string, $salt = ""){
+        if($salt == ""){return hash("XXXXXX", hash("XXXXXX", $string));}
+        else{return hash("XXXXXX", $string.hash("XXXXXX", $salt));}
+    }
