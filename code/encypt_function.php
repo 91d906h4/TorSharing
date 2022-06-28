11f1c271b6243f3dc2a41bbea0501181f3dd8039
91d906h4 <104009754+91d906h4@users.noreply.github.com>
2022-06-28 11:43:04 +0900

    Update encypt_function.php

--- code/encypt_function.php
+++ code/encypt_function.php
@@ -1,4 +1,4 @@
-    function encypt($string, $salt = ""){
-        if($salt == ""){return hash("XXXXXX", hash("XXXXXX", $string));}
-        else{return hash("XXXXXX", $string.hash("XXXXXX", $salt));}
-    }
+function encypt($string, $salt = ""){
+    if($salt == ""){return hash("XXXXXX", hash("XXXXXX", $string));}
+    else{return hash("XXXXXX", $string.hash("XXXXXX", $salt));}
+}
